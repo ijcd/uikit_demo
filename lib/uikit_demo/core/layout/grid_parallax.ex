0c0153b1a533322c876c6e413181e0f4e62472fc
Ian Duggan <ian@ianduggan.net>
2017-12-12 06:59:41 -0800

    Grid Parallax

--- lib/uikit_demo/core/layout/grid_parallax.ex
+++ lib/uikit_demo/core/layout/grid_parallax.ex
@@ -1,6 +1,7 @@
 defmodule UIKitDemo.Core.Layout.GridParallax do
   use Taggart.HTML, except: [table: 2]
   use UIKit 
+  import UIKitDemo.Core.Common
 
   def head_content do
     style(type: "text/css") do
@@ -23,142 +24,39 @@ defmodule UIKitDemo.Core.Layout.GridParallax do
   def demo_content do
     taggart do
       uk_container do
-        h1 do
-          "Grid Parallax"
-        end
-        h2 do
-          "3 Columns"
-        end
+        h1 "Grid Parallax"
+
+        h2 "3 Columns"
         div(class: "test uk-clearfix") do
           div(class: "uk-child-width-1-3@m", "uk-grid-parallax": "uk-grid-parallax") do
-            div do
-              div(class: "uk-panel uk-grid-margin")
-              div(class: "uk-panel uk-grid-margin")
-              div(class: "uk-panel uk-grid-margin")
-              div(class: "uk-panel uk-grid-margin")
-              div(class: "uk-panel uk-grid-margin")
-            end
-            div do
-              div(class: "uk-panel uk-grid-margin")
-              div(class: "uk-panel uk-grid-margin")
-              div(class: "uk-panel uk-grid-margin")
-              div(class: "uk-panel uk-grid-margin")
-              div(class: "uk-panel uk-grid-margin")
-            end
-            div do
-              div(class: "uk-panel uk-grid-margin")
-              div(class: "uk-panel uk-grid-margin")
-              div(class: "uk-panel uk-grid-margin")
-              div(class: "uk-panel uk-grid-margin")
-              div(class: "uk-panel uk-grid-margin")
+            for _x <- 1..3 do
+              div do
+                for _y <- 1..5 do
+                  div(class: "uk-panel uk-grid-margin")
+                end  
+              end
             end
           end
         end
-        h2 do
-          "Multi-row"
-        end
+
+        h2 "Multi-row"
         div(class: "test uk-clearfix") do
           div(class: "uk-child-width-1-2@ uk-child-width-1-3@m uk-child-width-1-3@l", "uk-grid-parallax": "translate:200") do
-            div do
-              div(class: "uk-panel")
-            end
-            div do
-              div(class: "uk-panel")
-            end
-            div do
-              div(class: "uk-panel")
-            end
-            div do
-              div(class: "uk-panel")
-            end
-            div do
-              div(class: "uk-panel")
-            end
-            div do
-              div(class: "uk-panel")
-            end
-            div do
-              div(class: "uk-panel")
-            end
-            div do
-              div(class: "uk-panel")
-            end
-            div do
-              div(class: "uk-panel")
-            end
-            div do
-              div(class: "uk-panel")
-            end
-            div do
-              div(class: "uk-panel")
-            end
-            div do
-              div(class: "uk-panel")
-            end
-            div do
-              div(class: "uk-panel")
-            end
-            div do
-              div(class: "uk-panel")
+            for _x <- 1..14 do
+              div uk_panel(do: "")
             end
           end
         end
-        h2 do
-          "Javascript Options"
-        end
-        div(class: "uk-overflow-auto") do
-          Taggart.HTML.table(class: "uk-table uk-table-striped") do
-            thead do
-              tr do
-                th do
-                  "Option"
-                end
-                th do
-                  "Value"
-                end
-                th do
-                  "Default"
-                end
-                th do
-                  "Description"
-                end
-              end
-            end
-            tbody do
-              tr do
-                td do
-                  code do
-                    "target"
-                  end
-                end
-                td do
-                  "String"
-                end
-                td do
-                  "false"
-                end
-                td do
-                  "Selector string for child elements to translate. Defaults to element's children."
-                end
-              end
-              tr do
-                td do
-                  code do
-                    "translate"
-                  end
-                end
-                td do
-                  "Number"
-                end
-                td do
-                  "150"
-                end
-                td do
-                  "Translate value. The value must be a positive integer."
-                end
-              end
-            end
-          end
+
+        h2 "Javascript Options"
+        uk(:div, overflow(:auto)) do          
+          striped_table(
+            ["Option", "Value", "Default", "Description"],
+            [
+              ["target", "String", "false", "Selector string for child elements to translate. Defaults to element's children."],
+              ["translate", "Number", "150", "Translate value. The value must be a positive integer."],
+            ]
+          )
         end
       end
     end
