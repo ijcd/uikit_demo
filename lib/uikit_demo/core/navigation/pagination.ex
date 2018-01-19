9080417423791bb8bc816d52576bb0d48f834817
Ian Duggan <ian@ianduggan.net>
2018-01-19 02:29:06 -0800

    Pagination

--- lib/uikit_demo/core/navigation/pagination.ex
+++ lib/uikit_demo/core/navigation/pagination.ex
@@ -1,6 +1,46 @@
 defmodule UIKitDemo.Core.Navigation.Pagination do
   use Taggart.HTML, except: [table: 2]
-  use UIKit 
+  use UIKit
+
+  def link_to(where) do
+    a(href: "#", do: where)
+  end
+
+  def pagination_numbers() do
+    taggart do
+      li(class: "uk-disabled") do
+        link_to uk_pagination_previous()
+      end
+      li link_to uk_pagination_previous()
+      li link_to "1"
+      li(class: "uk-disabled") do
+        span do
+          "..."
+        end
+      end
+      li link_to "4"
+      li link_to "5"
+      li link_to "6"
+      li(class: "uk-active") do
+        span do
+          "7"
+        end
+      end
+      li link_to "8"
+      li link_to "9"
+      li link_to "10"
+      li(class: "uk-disabled") do
+        span do
+          "..."
+        end
+      end
+      li link_to "20"
+      li link_to uk_pagination_next()
+      li(class: "uk-disabled") do
+        link_to uk_pagination_next()
+      end
+    end
+  end
 
   def demo_content do
     taggart do
@@ -8,265 +48,43 @@ defmodule UIKitDemo.Core.Navigation.Pagination do
         h1 do
           "Pagination"
         end
-        ul(class: "uk-pagination", "uk-margin": true) do
-          li(class: "uk-disabled") do
-            a(href: "#") do
-              span("uk-pagination-previous": true)
-            end
-          end
-          li do
-            a(href: "#") do
-              span("uk-pagination-previous": true)
-            end
-          end
-          li do
-            a(href: "#") do
-              "1"
-            end
-          end
-          li(class: "uk-disabled") do
-            span do
-              "..."
-            end
-          end
-          li do
-            a(href: "#") do
-              "4"
-            end
-          end
-          li do
-            a(href: "#") do
-              "5"
-            end
-          end
-          li do
-            a(href: "#") do
-              "6"
-            end
-          end
-          li(class: "uk-active") do
-            span do
-              "7"
-            end
-          end
-          li do
-            a(href: "#") do
-              "8"
-            end
-          end
-          li do
-            a(href: "#") do
-              "9"
-            end
-          end
-          li do
-            a(href: "#") do
-              "10"
-            end
-          end
-          li(class: "uk-disabled") do
-            span do
-              "..."
-            end
-          end
-          li do
-            a(href: "#") do
-              "20"
-            end
-          end
-          li do
-            a(href: "#") do
-              span("uk-pagination-next": true)
-            end
-          end
-          li(class: "uk-disabled") do
-            a(href: "#") do
-              span("uk-pagination-next": true)
-            end
-          end
+        uk_pagination(margin()) do
+          pagination_numbers()
         end
-        ul(class: "uk-pagination uk-flex-center", "uk-margin": true) do
-          li(class: "uk-disabled") do
-            a(href: "#") do
-              span("uk-pagination-previous": true)
-            end
-          end
-          li do
-            a(href: "#") do
-              span("uk-pagination-previous": true)
-            end
-          end
-          li do
-            a(href: "#") do
-              "1"
-            end
-          end
-          li(class: "uk-disabled") do
-            span do
-              "..."
-            end
-          end
-          li do
-            a(href: "#") do
-              "4"
-            end
-          end
-          li do
-            a(href: "#") do
-              "5"
-            end
-          end
-          li do
-            a(href: "#") do
-              "6"
-            end
-          end
-          li(class: "uk-active") do
-            span do
-              "7"
-            end
-          end
-          li do
-            a(href: "#") do
-              "8"
-            end
-          end
-          li do
-            a(href: "#") do
-              "9"
-            end
-          end
-          li do
-            a(href: "#") do
-              "10"
-            end
-          end
-          li(class: "uk-disabled") do
-            span do
-              "..."
-            end
-          end
-          li do
-            a(href: "#") do
-              "20"
-            end
-          end
-          li do
-            a(href: "#") do
-              span("uk-pagination-next": true)
-            end
-          end
-          li(class: "uk-disabled") do
-            a(href: "#") do
-              span("uk-pagination-next": true)
-            end
-          end
+        uk_pagination(flex(:center), margin()) do          
+          pagination_numbers()
         end
-        ul(class: "uk-pagination uk-flex-right", "uk-margin": true) do
-          li(class: "uk-disabled") do
-            a(href: "#") do
-              span("uk-pagination-previous": true)
-            end
-          end
-          li do
-            a(href: "#") do
-              span("uk-pagination-previous": true)
-            end
-          end
-          li do
-            a(href: "#") do
-              "1"
-            end
-          end
-          li(class: "uk-disabled") do
-            span do
-              "..."
-            end
-          end
-          li do
-            a(href: "#") do
-              "4"
-            end
-          end
-          li do
-            a(href: "#") do
-              "5"
-            end
-          end
-          li do
-            a(href: "#") do
-              "6"
-            end
-          end
-          li(class: "uk-active") do
-            span do
-              "7"
-            end
-          end
-          li do
-            a(href: "#") do
-              "8"
-            end
-          end
-          li do
-            a(href: "#") do
-              "9"
-            end
-          end
-          li do
-            a(href: "#") do
-              "10"
-            end
-          end
-          li(class: "uk-disabled") do
-            span do
-              "..."
-            end
-          end
-          li do
-            a(href: "#") do
-              "20"
-            end
-          end
-          li do
-            a(href: "#") do
-              span("uk-pagination-next": true)
-            end
-          end
-          li(class: "uk-disabled") do
-            a(href: "#") do
-              span("uk-pagination-next": true)
-            end
-          end
+        uk_pagination(flex(:right), margin()) do                    
+          pagination_numbers()
         end
         h2 do
           "Previous/Next"
         end
-        ul(class: "uk-pagination uk-flex-center") do
+        uk_pagination(flex(:center)) do                    
           li do
             a(href: "#") do
-              span("uk-pagination-previous": true)
+              uk_pagination_previous()
               " Previous"
             end
           end
           li do
             a(href: "#") do
               "Next "
-              span("uk-pagination-next": true)
+              uk_pagination_next()
             end
           end
         end
         ul(class: "uk-pagination") do
           li do
             a(href: "#") do
-              span("uk-pagination-previous": true)
+              uk_pagination_previous()
               " Previous"
             end
           end
           li(class: "uk-margin-auto-left") do
             a(href: "#") do
               "Next "
-              span("uk-pagination-next": true)
+              uk_pagination_next()
             end
           end
         end
