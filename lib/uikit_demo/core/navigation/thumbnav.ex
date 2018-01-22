ba7c865c721a791d4ca03067a9736610d73214dc
Ian Duggan <ian@ianduggan.net>
2018-01-22 03:10:17 -0800

    Thumbnav.

--- lib/uikit_demo/core/navigation/thumbnav.ex
+++ lib/uikit_demo/core/navigation/thumbnav.ex
@@ -2,6 +2,12 @@ defmodule UIKitDemo.Core.Navigation.Thumbnav do
   use Taggart.HTML, except: [table: 2]
   use UIKit 
 
+  def link_photo(what) do
+    a(href: "#") do
+      img(src: "/images/#{what}.jpg", width: "100", alt: "")
+    end
+  end
+
   def demo_content do
     taggart do
       uk_container do
@@ -11,48 +17,36 @@ defmodule UIKitDemo.Core.Navigation.Thumbnav do
         h2 do
           "Horizontal"
         end
-        ul(class: "uk-thumbnav") do
+        uk_thumbnav do
           li(class: "uk-active") do
-            a(href: "#") do
-              img(src: "/images/photo.jpg", width: "100", alt: "")
-            end
+            link_photo("photo")
           end
           li do
-            a(href: "#") do
-              img(src: "/images/dark.jpg", width: "100", alt: "")
-            end
+            link_photo("dark")
           end
           li do
-            a(href: "#") do
-              img(src: "/images/light.jpg", width: "100", alt: "")
-            end
+            link_photo("light")
           end
         end
         h2 do
           "Vertical"
         end
-        ul(class: "uk-thumbnav uk-thumbnav-vertical") do
+        uk_thumbnav(:vertical) do
           li(class: "uk-active") do
-            a(href: "#") do
-              img(src: "/images/photo.jpg", width: "100", alt: "")
-            end
+            link_photo("photo")
           end
           li do
-            a(href: "#") do
-              img(src: "/images/dark.jpg", width: "100", alt: "")
-            end
+            link_photo("dark")
           end
           li do
-            a(href: "#") do
-              img(src: "/images/light.jpg", width: "100", alt: "")
-            end
+            link_photo("light")
           end
         end
         h2 do
           "Image"
         end
-        div(class: "uk-position-relative", "uk-slideshow": "animation: fade") do
-          ul(class: "uk-slideshow-items") do
+        uk_slideshow(position(:relative), animation: :fade) do
+          uk_slideshow_items do
             li do
               img(src: "/images/photo.jpg", alt: "", "uk-cover": true)
             end
@@ -63,41 +57,29 @@ defmodule UIKitDemo.Core.Navigation.Thumbnav do
               img(src: "/images/light.jpg", alt: "", "uk-cover": true)
             end
           end
-          div(class: "uk-position-bottom-center uk-position-small") do
-            ul(class: "uk-thumbnav") do
-              li("uk-slideshow-item": "0") do
-                a(href: "#") do
-                  img(src: "/images/photo.jpg", width: "100", alt: "")
-                end
+          uk(:div, position(:bottom_center, :small)) do
+            uk_thumbnav do
+              uk_slideshow_item(0) do
+                link_photo("photo")
               end
-              li("uk-slideshow-item": "1") do
-                a(href: "#") do
-                  img(src: "/images/dark.jpg", width: "100", alt: "")
-                end
+              uk_slideshow_item(1) do
+                link_photo("dark")
               end
-              li("uk-slideshow-item": "2") do
-                a(href: "#") do
-                  img(src: "/images/light.jpg", width: "100", alt: "")
-                end
+              uk_slideshow_item(2) do
+                link_photo("light")
               end
             end
           end
-          div(class: "uk-position-center-right uk-position-small") do
-            ul(class: "uk-thumbnav uk-thumbnav-vertical") do
-              li("uk-slideshow-item": "0") do
-                a(href: "#") do
-                  img(src: "/images/photo.jpg", width: "100", alt: "")
-                end
+          uk(:div, position(:center_right, :small)) do
+            uk_thumbnav(:vertical) do
+              uk_slideshow_item(0) do
+                link_photo("photo")
               end
-              li("uk-slideshow-item": "1") do
-                a(href: "#") do
-                  img(src: "/images/dark.jpg", width: "100", alt: "")
-                end
+              uk_slideshow_item(1) do
+                link_photo("dark")
               end
-              li("uk-slideshow-item": "2") do
-                a(href: "#") do
-                  img(src: "/images/light.jpg", width: "100", alt: "")
-                end
+              uk_slideshow_item(2) do
+                link_photo("light")
               end
             end
           end
