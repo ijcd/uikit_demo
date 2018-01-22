92f46817cf05ea1eaf6cc870de99a05248bee01f
Ian Duggan <ian@ianduggan.net>
2018-01-22 02:48:13 -0800

    Tab.

--- lib/uikit_demo/core/navigation/tab.ex
+++ lib/uikit_demo/core/navigation/tab.ex
@@ -1,6 +1,31 @@
 defmodule UIKitDemo.Core.Navigation.Tab do
   use Taggart.HTML, except: [table: 2]
   use UIKit 
+  import UIKitDemo.Core.Common
+
+  def li_item do
+    li do
+      a(href: "#") do
+        "Item"
+      end
+    end
+  end
+
+  def li_disabled do
+    li(class: "uk-disabled") do
+      a(href: "#") do
+        "Disabled"
+      end
+    end    
+  end
+
+  def li_active(label) do
+    li(class: "uk-active") do
+      a(href: "#") do
+        label
+      end
+    end    
+  end
 
   def demo_content do
     taggart do
@@ -8,398 +33,146 @@ defmodule UIKitDemo.Core.Navigation.Tab do
         h1 do
           "Tab"
         end
-        div(class: "uk-child-width-1-2@m", "uk-grid": true) do
+        uk_grid(child_width("1-2@m")) do
           div do
             h2 do
               "Top"
             end
             ul("uk-tab": true) do
-              li(class: "uk-active") do
-                a(href: "#") do
-                  "Left"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li(class: "uk-disabled") do
-                a(href: "#") do
-                  "Disabled"
-                end
-              end
+              li_active("Left")
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_disabled()
             end
-            ul(class: "uk-flex-right", "uk-tab": true) do
-              li(class: "uk-active") do
-                a(href: "#") do
-                  "Right"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li(class: "uk-disabled") do
-                a(href: "#") do
-                  "Disabled"
-                end
-              end
+            uk_tab(flex(:right)) do
+              li_active("Right")
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_disabled()
             end
-            ul(class: "uk-flex-center", "uk-tab": true) do
-              li(class: "uk-active") do
-                a(href: "#") do
-                  "Center"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
+            uk_tab(flex(:center)) do
+              li_active("Center")
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
             end
-            ul(class: "uk-child-width-1-5", "uk-tab": true) do
-              li(class: "uk-active") do
-                a(href: "#") do
-                  "Justify"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
+            uk_tab(child_width("1-5")) do
+              li_active("Justify")
+              li_item()
+              li_item()
+              li_item()
+              li_item()
             end
           end
           div do
             h2 do
               "Bottom"
             end
-            ul(class: "uk-tab-bottom", "uk-tab": true) do
-              li(class: "uk-active") do
-                a(href: "#") do
-                  "Left"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li(class: "uk-disabled") do
-                a(href: "#") do
-                  "Disabled"
-                end
-              end
+            uk_tab(:bottom) do
+              li_active("Left")
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_disabled()
             end
-            ul(class: "uk-tab-bottom uk-flex-right", "uk-tab": true) do
-              li(class: "uk-active") do
-                a(href: "#") do
-                  "Right"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li(class: "uk-disabled") do
-                a(href: "#") do
-                  "Disabled"
-                end
-              end
+            uk_tab(:bottom, flex(:right)) do
+              li_active("Right")
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_disabled()
             end
-            ul(class: "uk-tab-bottom uk-flex-center", "uk-tab": true) do
-              li(class: "uk-active") do
-                a(href: "#") do
-                  "Center"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
+            uk_tab(:bottom, flex(:center)) do
+              li_active("Center")
+              li_item()
+              li_item()
+              li_item()
+              li_item()
             end
-            ul(class: "uk-tab-bottom uk-child-width-1-5", "uk-tab": true) do
-              li(class: "uk-active") do
-                a(href: "#") do
-                  "Justify"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
+            uk_tab(:bottom, child_width("1-5")) do
+              li_active("Justify")
+              li_item()
+              li_item()
+              li_item()
+              li_item()
             end
           end
         end
         h2 do
           "Left/Right"
         end
-        div(class: "uk-child-width-auto", "uk-grid": true) do
+        uk_grid(child_width("auto")) do
           div do
-            ul(class: "uk-tab-left", "uk-tab": true) do
-              li(class: "uk-active") do
-                a(href: "#") do
-                  "Active"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
+            uk_tab(:left) do
+              li_active("Active")
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
             end
           end
           div do
-            ul(class: "uk-tab-right", "uk-tab": true) do
-              li(class: "uk-active") do
-                a(href: "#") do
-                  "Active"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
+            uk_tab(:right) do
+              li_active("Active")
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
             end
           end
         end
         h2 do
           "Dropdown"
         end
-        div(class: "uk-margin") do
-          ul(class: "uk-width-1-2@m", "uk-tab": true) do
-            li(class: "uk-active") do
-              a(href: "#") do
-                "Active"
-              end
-            end
-            li do
-              a(href: "#") do
-                "Item"
-              end
-            end
-            li do
-              a(href: "#") do
-                "Item"
-              end
-            end
+        uk_margin do
+          uk_tab(width("1-2@m")) do
+            li_active("Active")
+            li_item()
+            li_item()
             li do
               a(href: "#") do
                 "More "
-                span(class: "uk-margin-small-left", "uk-icon": "icon: trash")
+                uk_icon(:trash, margin(:small_left))
               end
-              div("uk-dropdown": "mode: click") do
-                ul(class: "uk-nav uk-dropdown-nav") do
-                  li(class: "uk-active") do
-                    a(href: "#") do
-                      "Active"
-                    end
-                  end
-                  li(class: "uk-parent") do
+              uk_dropdown(mode: "click") do
+                uk_nav(dropdown_nav()) do
+                  li_active("Active")
+                  uk(:li, parent()) do
                     a(href: "#") do
                       "Item"
                     end
                   end
-                  li(class: "uk-nav-header") do
+                  uk_nav_header() do
                     "Header"
                   end
                   li do
                     a(href: "#") do
-                      span(class: "uk-margin-small-right", "uk-icon": "icon: table")
+                      uk_icon(:table, margin(:small_right))
                       " Item"
                     end
                   end
                   li do
                     a(href: "#") do
-                      span(class: "uk-margin-small-right", "uk-icon": "icon: thumbnails")
+                      uk_icon(:thumbnails, margin(:small_right))
                       " Item"
                     end
                   end
-                  li(class: "uk-nav-divider")
+                  uk_nav_divider()
                   li do
                     a(href: "#") do
-                      span(class: "uk-margin-small-right", "uk-icon": "icon: trash")
+                      uk_icon(:trash, margin(:small_right))
                       " Item"
                     end
                   end
@@ -408,142 +181,21 @@ defmodule UIKitDemo.Core.Navigation.Tab do
             end
           end
         end
-        h2 do
-          "Javascript Options"
-        end
-        div(class: "uk-overflow-auto") do
-          Taggart.HTML.table(class: "uk-table uk-table-striped") do
-            thead do
-              tr do
-                th do
-                  "Option"
-                end
-                th do
-                  "Value"
-                end
-                th do
-                  "Default"
-                end
-                th do
-                  "Description"
-                end
-              end
-            end
-            tbody do
-              tr do
-                td do
-                  code do
-                    "connect"
-                  end
-                end
-                td do
-                  "CSS selector"
-                end
-                td do
-                  "false"
-                end
-                td do
-                  "Related items container. By default next element with class 'uk-switcher'."
-                end
-              end
-              tr do
-                td do
-                  code do
-                    "toggle"
-                  end
-                end
-                td do
-                  "CSS selector"
-                end
-                td do
-                  "> *"
-                end
-                td do
-                  "Toggle selector - triggers content switching on click."
-                end
-              end
-              tr do
-                td do
-                  code do
-                    "active"
-                  end
-                end
-                td do
-                  "Number"
-                end
-                td do
-                  "0"
-                end
-                td do
-                  "Active index on init. (Providing a negative number indicates a position starting from the end of the set)"
-                end
-              end
-              tr do
-                td do
-                  code do
-                    "animation"
-                  end
-                end
-                td do
-                  "String"
-                end
-                td do
-                  "false"
-                end
-                td do
-                  "The space separated names of animations to use. (Comma separate for animation out)"
-                end
-              end
-              tr do
-                td do
-                  code do
-                    "duration"
-                  end
-                end
-                td do
-                  "Number"
-                end
-                td do
-                  "200"
-                end
-                td do
-                  "The animation duration."
-                end
-              end
-              tr do
-                td do
-                  code do
-                    "swiping"
-                  end
-                end
-                td do
-                  "Boolean"
-                end
-                td do
-                  "true"
-                end
-                td do
-                  "Use swiping."
-                end
-              end
-              tr do
-                td do
-                  code do
-                    "media"
-                  end
-                end
-                td do
-                  "Integer|String"
-                end
-                td do
-                  "960"
-                end
-                td do
-                  "When to become horizontal - a width as integer (e.g. 640) or a breakpoint (e.g. @s, @m, @l, @xl)."
-                end
-              end
-            end
-          end
+
+        h2 "Javascript Options"
+        uk(:div, overflow(:auto)) do
+          striped_table(
+            ["Option", "Value", "Default", "Description"],
+            [
+              ["connect", "CSS selector", "false", "Related items container. By default next element with class 'uk-switcher'."],
+              ["toggle", "CSS selector", "> *", "Toggle selector - triggers content switching on click."],
+              ["active", "Number", "0", "Active index on init. (Providing a negative number indicates a position starting from the end of the set)"],
+              ["animation", "String", "false", "The space separated names of animations to use. (Comma separate for animation out)"],
+              ["duration", "Number", "200", "The animation duration."],
+              ["swiping", "Boolean", "true", "Use swiping."],
+              ["media", "Integer|String", "960", "When to become horizontal - a width as integer (e.g. 640) or a breakpoint (e.g. @s, @m, @l, @xl)."],
+            ]
+          )
         end
       end
     end
