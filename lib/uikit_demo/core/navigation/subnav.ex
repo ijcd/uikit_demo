987e54786f7953aa05a630eefe136925fc4f4ad7
Ian Duggan <ian@ianduggan.net>
2018-01-22 01:43:35 -0800

    Subnav.

--- lib/uikit_demo/core/navigation/subnav.ex
+++ lib/uikit_demo/core/navigation/subnav.ex
@@ -2,6 +2,38 @@ defmodule UIKitDemo.Core.Navigation.Subnav do
   use Taggart.HTML, except: [table: 2]
   use UIKit 
 
+  def li_item do
+    li do
+      a(href: "#") do
+        "Item"
+      end
+    end
+  end
+
+  def li_active do
+    li(class: "uk-active") do
+      a(href: "#") do
+        "Active"
+      end
+    end
+  end
+
+  def li_disabled do
+    li(class: "uk-disabled") do
+      a(href: "#") do
+        "Disabled"
+      end
+    end
+  end
+
+  def li_disabled_span do
+    li do
+      span do
+        "Disabled"
+      end
+    end
+  end
+
   def demo_content do
     taggart do
       uk_container do
@@ -10,328 +42,90 @@ defmodule UIKitDemo.Core.Navigation.Subnav do
         end
         div(class: "uk-grid uk-child-width-1-2@m") do
           div do
-            ul(class: "uk-subnav", "uk-margin": true) do
-              li(class: "uk-active") do
-                a(href: "#") do
-                  "Active"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li(class: "uk-disabled") do
-                a(href: "#") do
-                  "Disabled"
-                end
-              end
+            uk_subnav(child_margins()) do
+              li_active()
+              li_item()
+              li_item()
+              li_disabled()
             end
             h2 do
               "Divider"
             end
-            ul(class: "uk-subnav uk-subnav-divider", "uk-margin": true) do
-              li(class: "uk-active") do
-                a(href: "#") do
-                  "Active"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                span do
-                  "Disabled"
-                end
-              end
+            # ul(class: "uk-subnav uk-subnav-divider", "uk-margin": true) do
+            uk_subnav(:divider, child_margins()) do
+              li_active()
+              li_item()
+              li_item()
+              li_disabled_span()
             end
             h2 do
               "Pill"
             end
-            ul(class: "uk-subnav uk-subnav-pill", "uk-margin": true) do
-              li(class: "uk-active") do
-                a(href: "#") do
-                  "Active"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                span do
-                  "Disabled"
-                end
-              end
+            uk_subnav(:pill, child_margins()) do
+              li_active()
+              li_item()
+              li_item()
+              li_disabled_span()
             end
           end
           div do
             h2 do
               "Wrap"
             end
-            ul(class: "uk-subnav", "uk-margin": true) do
-              li(class: "uk-active") do
-                a(href: "#") do
-                  "Active"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
+            uk_subnav(child_margins()) do
+              li_active()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
             end
-            ul(class: "uk-subnav uk-subnav-divider", "uk-margin": true) do
-              li(class: "uk-active") do
-                a(href: "#") do
-                  "Active"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
+            # ul(class: "uk-subnav uk-subnav-divider", "uk-margin": true) do
+            uk_subnav(:divider, child_margins()) do
+              li_active()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
             end
-            ul(class: "uk-subnav uk-subnav-pill", "uk-margin": true) do
-              li(class: "uk-active") do
-                a(href: "#") do
-                  "Active"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
-              li do
-                a(href: "#") do
-                  "Item"
-                end
-              end
+            uk_subnav(:pill, child_margins()) do
+              li_active()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
+              li_item()
             end
           end
         end
