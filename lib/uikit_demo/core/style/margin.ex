92dbf1d9cca6ee39ad3029073d46e09fb7d48069
Ian Duggan <ian@ianduggan.net>
2018-01-22 04:31:26 -0800

    Margin.

--- lib/uikit_demo/core/style/margin.ex
+++ lib/uikit_demo/core/style/margin.ex
@@ -1,6 +1,7 @@
 defmodule UIKitDemo.Core.Style.Margin do
   use Taggart.HTML, except: [table: 2]
   use UIKit
+  import UIKitDemo.Core.Common
 
   def head_content do
     style(type: "text/css") do
@@ -15,299 +16,147 @@ defmodule UIKitDemo.Core.Style.Margin do
     end
   end
 
+  def lorem do
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat."
+  end
+
+  def lorem_ulamaco do
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
+  end
+
+  def a_button do
+    button(class: "uk-button uk-button-default") do
+      "Button"
+    end
+  end
+
   def demo_content do
     taggart do
       uk_container do
-        h1 do
-          "Margin"
-        end
-        div(class: "uk-child-width-1-2@m", "uk-grid": true) do
+        h1 "Margin"
+        uk_grid(child_width("1-2@m")) do
           div do
-            div(class: "uk-margin") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat."
+            uk_margin do
+              lorem()
             end
-            div(class: "uk-margin") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat."
+            uk_margin do
+              lorem()
             end
           end
           div do
-            div(class: "uk-margin uk-panel") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
+            uk_margin(panel()) do
+              lorem_ulamaco()
             end
-            div(class: "uk-margin uk-panel") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
-            end
-          end
-        end
-        h2 do
-          "Small"
-        end
-        div(class: "uk-child-width-1-2@m", "uk-grid": true) do
-          div do
-            div(class: "uk-margin-small") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat."
-            end
-            div(class: "uk-margin-small") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat."
-            end
-          end
-          div do
-            div(class: "uk-margin-small uk-panel") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
-            end
-            div(class: "uk-margin-small uk-panel") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
+            uk_margin(panel()) do
+              lorem_ulamaco()
             end
           end
         end
-        h2 do
-          "Medium"
-        end
-        div(class: "uk-child-width-1-2@m", "uk-grid": true) do
+
+        h2 "Small"
+        uk_grid(child_width("1-2@m")) do
           div do
-            div(class: "uk-margin-medium") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat."
+            uk_margin(:small) do
+              lorem()
             end
-            div(class: "uk-margin-medium") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat."
+            uk_margin(:small) do
+              lorem()
             end
           end
           div do
-            div(class: "uk-margin-medium uk-panel") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
+            uk_margin(:small, panel()) do
+              lorem_ulamaco()
             end
-            div(class: "uk-margin-medium uk-panel") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
-            end
-          end
-        end
-        h2 do
-          "Large"
-        end
-        div(class: "uk-child-width-1-2@m", "uk-grid": true) do
-          div do
-            div(class: "uk-margin-large") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat."
-            end
-            div(class: "uk-margin-large") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat."
-            end
-          end
-          div do
-            div(class: "uk-margin-large uk-panel") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
-            end
-            div(class: "uk-margin-large uk-panel") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
+            uk_margin(:small, panel()) do
+              lorem_ulamaco()
             end
           end
         end
-        h2 do
-          "XLarge"
-        end
-        div(class: "uk-child-width-1-2@m", "uk-grid": true) do
+
+        h2 "Medium"
+        uk_grid(child_width("1-2@m")) do
           div do
-            div(class: "uk-margin-xlarge") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat."
+            uk_margin(:medium) do
+              lorem()
             end
-            div(class: "uk-margin-xlarge") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat."
+            uk_margin(:medium) do
+              lorem()
             end
           end
           div do
-            div(class: "uk-margin-xlarge uk-panel") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
+            uk_margin(:medium, panel()) do
+              lorem_ulamaco()
             end
-            div(class: "uk-margin-xlarge uk-panel") do
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco."
-            end
-          end
-        end
-        h2 do
-          "Wrap"
-        end
-        div(class: "uk-child-width-1-2@m", "uk-grid": true) do
-          div("uk-margin": true) do
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-          end
-          div("uk-margin": "margin: uk-margin-top") do
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
-            end
-            " "
-            button(class: "uk-button uk-button-default") do
-              "Button"
+            uk_margin(:medium, panel()) do
+              lorem_ulamaco()
             end
           end
         end
-        h2 do
-          "Javascript Options"
-        end
-        div(class: "uk-overflow-auto") do
-          Taggart.HTML.table(class: "uk-table uk-table-striped") do
-            thead do
-              tr do
-                th do
-                  "Option"
-                end
-                th do
-                  "Value"
-                end
-                th do
-                  "Default"
-                end
-                th do
-                  "Description"
-                end
-              end
+
+        h2 "Large"
+        uk_grid(child_width("1-2@m")) do
+          div do
+            uk_margin(:large) do
+              lorem()
             end
-            tbody do
-              tr do
-                td do
-                  code do
-                    "margin"
-                  end
-                end
-                td do
-                  "String"
-                end
-                td do
-                  "uk-margin-small-top"
-                end
-                td do
-                  "The margin class."
-                end
-              end
-              tr do
-                td do
-                  code do
-                    "first-column"
-                  end
-                end
-                td do
-                  "String"
-                end
-                td do
-                  "uk-first-column"
-                end
-                td do
-                  "Class added to the first element in each row."
-                end
-              end
+            uk_margin(:large) do
+              lorem()
             end
           end
+          div do
+            uk_margin(:large, panel()) do
+              lorem_ulamaco()
+            end
+            uk_margin(:large, panel()) do
+              lorem_ulamaco()
+            end
+          end
+        end
+
+        h2 "XLarge"
+        uk_grid(child_width("1-2@m")) do
+          div do
+            uk_margin(:xlarge) do
+              lorem()
+            end
+            uk_margin(:xlarge) do
+              lorem()
+            end
+          end
+          div do
+            uk_margin(:xlarge, panel()) do
+              lorem_ulamaco()
+            end
+            uk_margin(:xlarge, panel()) do
+              lorem_ulamaco()
+            end
+          end
+        end
+
+        h2 "Wrap"
+        uk_grid(child_width("1-2@m")) do
+          uk(:div, child_margins()) do
+            for _ <- 1..15 do
+              [" ", a_button()]
+            end
+          end
+          # TODO: dynamic child margins is a bit awkward still
+          uk(:div, attr(uk_margin: "margin: uk-margin-top")) do
+            for _ <- 1..15 do
+              [" ", a_button()]
+            end
+          end
+        end
+
+        h2 "Javascript Options"
+        uk(:div, overflow(:auto)) do
+          striped_table(
+            ["Option", "Value", "Default", "Description"],
+            [
+              ["margin", "String", "uk-margin-small-top", "The margin class."],
+              ["first-column", "String", "uk-first-column", "Class added to the first element in each row."],
+            ]
+          )
         end
       end
     end
